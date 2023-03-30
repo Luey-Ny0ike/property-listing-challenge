4a8c1366b501be781e2e2bd3d273a5671ab00231
Luey-Ny0ike <nyoikelewis@gmail.com>
2023-03-30 23:28:19 +0300

    Destroy associated records as well

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,8 +2,8 @@ class Property < ApplicationRecord
   PROPERTY_TYPES = ['Apartment', 'House', 'Townhouse', 'Condo'].freeze
 
   # Associations
-  has_one :furnishing
-  has_one :valuation
+  has_one :furnishing, dependent: :destroy
+  has_one :valuation, dependent: :destroy
 
   # Validations
   validates_presence_of :name, :property_type, :description
