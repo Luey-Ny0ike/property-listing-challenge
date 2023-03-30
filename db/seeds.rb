90f0c4038285320fd9cc97e1b22183ceaa161181
Luey-Ny0ike <nyoikelewis@gmail.com>
2023-03-30 23:57:02 +0300

    Added seed data to help setup easily

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do
+  FactoryBot.create(:property)
+end
+
+15.times do
+  FactoryBot.create(:valuation, {property: Property.all.sample})
+end
+
+15.times do
+  FactoryBot.create(:furnishing, {property: Property.all.sample})
+end
