e5615a98393695e0b0197d7dd6467c07bbba87ed
Luey-Ny0ike <nyoikelewis@gmail.com>
2023-03-30 22:52:14 +0300

    Mutation to destroy a furnishing record

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :delete_furnishing, mutation: Mutations::DeleteFurnishing
     field :create_furnishing, mutation: Mutations::CreateFurnishing
     field :create_property, mutation: Mutations::CreateProperty
     field :delete_property, mutation: Mutations::DeleteProperty
