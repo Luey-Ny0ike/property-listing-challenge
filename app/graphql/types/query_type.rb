c66696ca81eaa0441578b8b4539f21eee61d29f1
Luey-Ny0ike <nyoikelewis@gmail.com>
2023-03-30 20:05:11 +0300

    Wrote the queries to retrieve collections and single data

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,55 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :furnishings, [FurnishingType], null: false do
+      description 'Retrieves all the furnishings'
+    end
+
+    def furnishings
+      Furnishing.all
+    end
+
+    field :furnishing, FurnishingType do
+      description 'Retrieves single furnishing'
+      argument :id, ID, required: true
+    end
+
+    def furnishing(id:)
+      Furnishing.find(id)
+    end
+
+    field :properties, [PropertyType], null: false do
+      description 'Retrieves all the properties'
+    end
+
+    def properties
+      Property.all
+    end
+
+    field :property, PropertyType do
+      description 'Retrieves a single property'
+      argument :id, ID, required: true
+    end
+
+    def property(id:)
+      Property.find(id)
+    end
+
+    field :valuations, [ValuationType], null: false do
+      description 'Retrieve all valuations'
+    end
+
+    def valuations
+      Valuation.all
+    end
+
+    field :valuation, ValuationType do
+      description 'Retrieves a single valuation'
+      argument :id, ID, required: true
+    end
+
+    def valuation(id:)
+      Valuation.find(id)
     end
   end
 end
