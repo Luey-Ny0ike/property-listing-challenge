a0aeb693063e04bbc32d1fc5e01044fbfaee9861
Luey-Ny0ike <nyoikelewis@gmail.com>
2023-03-30 20:04:03 +0300

    Defined the furnishing type

---
+++ app/graphql/types/furnishing_type.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Types
+  class FurnishingType < Types::BaseObject
+    field :id, ID, null: false
+    field :property_id, Integer, null: false
+    field :description, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :property, PropertyType, null: false
+  end
+
+  def property
+    object.property
+  end
+end
